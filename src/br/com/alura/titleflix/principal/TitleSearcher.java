c41cdaf928ddce00aa65ff2444927bed26ba792e
Estevam Rainer <estevamrainerdev@gmail.com>
2023-08-14 11:41:48 -0300

    Fix: Editando o nome de 'Principal com Java' para 'TitleSearcher'

--- src/br/com/alura/titleflix/principal/TitleSearcher.java
+++ src/br/com/alura/titleflix/principal/TitleSearcher.java
@@ -15,7 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class PrincipalComBusca {
+public class TitleSearcher {
     public static void main(String[] args) throws IOException, InterruptedException {
         Scanner scanner = new Scanner(System.in);
         String busca = " ";
